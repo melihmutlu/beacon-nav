6d402b83e51d4853d8fe15140a1fe26ccbbd8b73
Melih Mutlu <m.melihmutlu@gmail.com>
2016-06-23 12:05:58 +0300

    scanning devices

--- app/src/main/java/com/example/melih/beacon_nav/MainActivity.java
+++ app/src/main/java/com/example/melih/beacon_nav/MainActivity.java
@@ -12,18 +12,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_FINISHED;
-import static android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_STARTED;
 import static android.bluetooth.BluetoothAdapter.getDefaultAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> address ;
+    private ListView  listView;
+    private boolean state = true;
+    private ArrayAdapter adapter;
+    private ArrayList<String> results ;
+    private double txPower = -8.0;
     private String beaconAddress ;
     private int counter = 0;
     private BluetoothAdapter mBTAdapter;
@@ -31,24 +36,31 @@ public class MainActivity extends AppCompatActivity {
     private TextView text ;
     private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
+            Log.d("INFO", "searching");
             address.add("D0:30:AD:84:07:40");
             address.add("E0:2E:E2:ED:86:64");
             address.add("D0:8B:08:63:C4:61");
             address.add("FC:73:08:31:50:42");
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                Log.d("INFO", "Device found");
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 int  rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
                 Log.d("DEVICES", "Device: " + device.getName() + " " + rssi + " " + device.getAddress());
                 beaconAddress = device.getAddress();
                 if(address.contains(beaconAddress)){
+                    Log.d("INFO", "Device found");
                     Log.d("DEVICES", "Device: " + device.getName() + " " + rssi + " " + device.getAddress());
-                    text.setText(device.getAddress() + "\n " + rssi );
+                    double dist = calculateAccuracy(rssi);
+                    results.add(device.getAddress() + "\n " + rssi + "\n " + dist);
+                    adapter = new ArrayAdapter<>(MainActivity.this, R.layout.main_list, results);
+                    listView.setAdapter(adapter);
+
                 }
-            }else if(ACTION_DISCOVERY_FINISHED.equals(action)){
-                mBTAdapter.startDiscovery();
-            }else if(ACTION_DISCOVERY_STARTED.equals(action)){
+            }else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.d("INFO", "finished");
+            }else if( BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)){
+                counter = 0;
+                results.clear();
             }
             address.clear();
         }
@@ -59,9 +71,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanBtn = (Button) findViewById(R.id.scanBtn);
-        text = (TextView) findViewById(R.id.deviceName);
         address = new ArrayList<>();
+        results = new ArrayList<>();
         mBTAdapter = getDefaultAdapter();
+        listView = (ListView) findViewById(R.id.list);
 
         if(!mBTAdapter.isEnabled())
             mBTAdapter.enable();
@@ -78,20 +91,21 @@ public class MainActivity extends AppCompatActivity {
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
         }
-    scanBtn.setOnClickListener(new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            if(mBTAdapter.isDiscovering())
-                mBTAdapter.cancelDiscovery();
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            filter.addAction(ACTION_DISCOVERY_FINISHED);
-            filter.addAction(ACTION_DISCOVERY_STARTED);
-            registerReceiver(bReceiver, filter);
-            Log.d("INFO", "başladı ");
-            mBTAdapter.startDiscovery();
-        }});
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("INFO", "clicked");
+                if(mBTAdapter.isDiscovering())
+                    mBTAdapter.cancelDiscovery();
+                Log.d("INFO", "start");
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+                filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+                registerReceiver(bReceiver, filter);
+                mBTAdapter.startDiscovery();
+            }});
     }
+
 
     @Override
     public void onDestroy() {
@@ -99,5 +113,10 @@ public class MainActivity extends AppCompatActivity {
             mBTAdapter.cancelDiscovery();
         unregisterReceiver(bReceiver);
         super.onDestroy();
+    }
+
+    protected double calculateAccuracy(double rssi) {
+        return Math.pow(10d, ( txPower - rssi) / (10 * 2));
+
     }
 }
