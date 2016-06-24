2c8eee3806fc325ea02128adb86a20f2d40baded
Melih Mutlu <m.melihmutlu@gmail.com>
2016-06-24 11:16:43 +0300

    device map

--- app/src/main/java/com/example/melih/beacon_nav/MainActivity.java
+++ app/src/main/java/com/example/melih/beacon_nav/MainActivity.java
@@ -6,9 +6,6 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_FINISHED;
@@ -27,8 +24,8 @@ import static android.bluetooth.BluetoothAdapter.getDefaultAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> results;
-    private double txPower = -8.0;
+    HashMap<String, ScanResult> listItems=new HashMap<String, ScanResult>();
+    private double txPower = -59.0;
     private BluetoothAdapter BTAdapter;
     private BluetoothLeScanner BTLE;
     private ArrayAdapter adapter;
@@ -36,33 +33,12 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private List<ScanResult> resultLE;
 
-    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                Log.d("DEVICES", "Device: " + device.getName() + " " + rssi + " " + device.getAddress());
-                double dist = calculateAccuracy(rssi);
-                results.add(device.getAddress() + "\n " + rssi + "\n " + dist);
-                adapter = new ArrayAdapter<>(MainActivity.this, R.layout.main_list, results);
-                listView.setAdapter(adapter);
-            }else if(ACTION_DISCOVERY_FINISHED.equals(action)){
-                Log.d("INFO", "finished");
-            }else if(ACTION_DISCOVERY_STARTED.equals(action)){
-                results.clear();
-            }
-        }
-
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanBtn = (Button) findViewById(R.id.scanBtn);
         listView = (ListView) findViewById(R.id.list);
-        results = new ArrayList<>();
         BTAdapter = getDefaultAdapter();
         BTLE = BTAdapter.getBluetoothLeScanner();
 
@@ -78,22 +54,20 @@ public class MainActivity extends AppCompatActivity {
                 filter.addAction(ACTION_DISCOVERY_FINISHED);
                 filter.addAction(ACTION_DISCOVERY_STARTED);
                 //registerReceiver(bReceiver, filter);
-                Log.d("INFO", "başladı ");
+                Log.d("INFO", "start ");
 
-                ScanCallback scan = new ScanCallback() {
+                ScanCallback scanCallback = new ScanCallback() {
                     @Override
                     public void onScanResult(int callbackType, ScanResult result) {
-                        results.add("device: " + result.getDevice() + ", rss: " + result.getRssi());
-                        adapter = new ArrayAdapter<>(MainActivity.this, R.layout.main_list, results);
+                        listItems.put(result.getDevice().getAddress() , result);
+                        //TODO custom adapter
+                        //adapter = new ArrayAdapter<>(MainActivity.this, R.layout.main_list, listItems);
                         listView.setAdapter(adapter);
-
                         Log.d("INFO", "device: " + result.getDevice() + ", rss: " + result.getRssi());
                     }
                 };
-                BTLE.startScan(scan);
-                scan.onBatchScanResults(resultLE);
-
-                //BTAdapter.startDiscovery();
+                BTLE.startScan(scanCallback);
+                scanCallback.onBatchScanResults(resultLE);
 
             }});
     }
@@ -104,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy() {
         if(BTAdapter.isDiscovering())
             BTAdapter.cancelDiscovery();
-        unregisterReceiver(bReceiver);
         super.onDestroy();
     }
 
