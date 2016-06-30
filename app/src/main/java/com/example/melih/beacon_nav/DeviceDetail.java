d4e6a864808cbf4484fe888b096471efd3984169
Melih Mutlu <m.melihmutlu@gmail.com>
2016-06-30 15:42:15 +0300

    calculations

--- app/src/main/java/com/example/melih/beacon_nav/DeviceDetail.java
+++ app/src/main/java/com/example/melih/beacon_nav/DeviceDetail.java
@@ -21,22 +21,39 @@ public class DeviceDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        rssi = getIntent().getIntegerArrayListExtra();
+        rssi = getIntent().getIntegerArrayListExtra("rssi");
         byte[] record = getIntent().getByteArrayExtra("tx");
         avg = (TextView) findViewById(R.id.avg);
         std_dev = (TextView) findViewById(R.id.std_dev);
         est_dist = (TextView) findViewById(R.id.est_dist);
 
-        byte[] sr = mDevices.get(position).getValue().getScanRecord().getBytes();
         try{
-            txValue = String.format("%02x ", sr[29]);
+            txValue = String.format("%02x ", record[29]);
         }catch (NullPointerException e){
             Log.d("DeviceDetailActivity", e.toString());
         }
 
-        avg.setText(getAverage());
-        std_dev.setText(getDeviation());
-        est_dist.setText(MainActivity.calculateDistance(getAverage));
+        avg.setText(""+ getAverage());
+        std_dev.setText( ""+ getDeviation());
+        est_dist.setText(MainActivity.calculateDistance();
 
     }
+
+    private double getAverage(){
+        double total =0;
+        for(int i : rssi){
+            total += i;
+        }
+        return total/rssi.size();
+    }
+
+    private double getDeviation(){
+        double mean = getAverage();
+        double total = 0;
+        for(int i: rssi){
+            total += (i- mean)*(i- mean);
+        }
+        return Math.sqrt(total/rssi.size());
+    }
+
 }
